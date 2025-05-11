a8cd210f973b74e7229473b98383aeebe64bcbc1
Vicsar71 <2005vtorres@gmail.com>
2025-05-12 00:11:37 +0200

    diseño login

--- src/poiupv/LoginController.java
+++ src/poiupv/LoginController.java
@@ -7,10 +7,10 @@ import javafx.scene.control.*;
 import javafx.scene.Scene;
 import javafx.scene.Parent;
 import javafx.stage.Stage;
+import javafx.scene.layout.VBox;
 import model.Navigation;
 import model.User;
 import model.NavDAOException;
-
 import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -22,11 +22,54 @@ public class LoginController implements Initializable {
     @FXML private Label loginErrorLabel;
     @FXML private Button loginButton;
     @FXML private Hyperlink registerLink;
+    @FXML private VBox leftContentBox;
+    @FXML private VBox loginVBox;
 
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         loginErrorLabel.setVisible(false);
+
+        loginVBox.sceneProperty().addListener((obs, oldScene, newScene) -> {
+            if (newScene != null) {
+                bindScaling(newScene);
+            }
+        });
     }
+
+    private void bindScaling(Scene scene) {
+        double baseWidth = 900;
+        double baseHeight = 600;
+        double maxScale = 1.3;
+
+        scene.widthProperty().addListener((obs, oldVal, newVal) -> {
+            double scale = Math.min(newVal.doubleValue() / baseWidth, maxScale);
+            loginVBox.setScaleX(scale);
+        });
+
+        scene.heightProperty().addListener((obs, oldVal, newVal) -> {
+            double scale = Math.min(newVal.doubleValue() / baseHeight, maxScale);
+            loginVBox.setScaleY(scale);
+            loginVBox.setSpacing(15 * scale);
+            
+        });
+        scene.widthProperty().addListener((obs, oldVal, newVal) -> {
+            double scale = Math.min(newVal.doubleValue() / baseWidth, maxScale);
+            loginVBox.setScaleX(scale);
+            leftContentBox.setScaleX(scale); // SOLO el logo y el texto
+        });
+
+        scene.heightProperty().addListener((obs, oldVal, newVal) -> {
+            double scale = Math.min(newVal.doubleValue() / baseHeight, maxScale);
+            loginVBox.setScaleY(scale);
+            loginVBox.setSpacing(15 * scale);
+
+            leftContentBox.setScaleY(scale);
+            leftContentBox.setSpacing(20 * scale);
+        });
+
+}
+
+
 
     @FXML
     private void handleLogin() {
